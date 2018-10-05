9dcf6f32ecfd0bcd6101dcd94fff3a25e8685f74
Tadashi Okawara <tadashi-okawara@cookpad.com>
2018-10-05 10:47:31 +0900

    pass test3

--- interp.rb
+++ interp.rb
@@ -24,6 +24,12 @@ def evaluate(exp, env)
     evaluate(exp[1], env) / evaluate(exp[2], env)
   when "%"
     evaluate(exp[1], env) % evaluate(exp[2], env)
+  when ">"
+    evaluate(exp[1], env) > evaluate(exp[2], env)
+  when "<"
+    evaluate(exp[1], env) < evaluate(exp[2], env)
+  when "=="
+    evaluate(exp[1], env) == evaluate(exp[2], env)
   # ... Implement other operators that you need
 
 #
@@ -71,11 +77,17 @@ def evaluate(exp, env)
     #   else
     #     ???
     #   end
-    raise(NotImplementedError) # Problem 3
+    if evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    else
+      evaluate(exp[3], env)
+    end
 
   when "while"
     # Loop.
-    raise(NotImplementedError) # Problem 3
+    while evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    end
 
 
 #
